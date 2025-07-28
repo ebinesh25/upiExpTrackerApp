667d2d270ee8363d96628891f5c172760d3eab84
Positron <positron@pop-os.inanga-godzilla.ts.net>
2025-07-28 15:58:14 +0530

    UPI app init with form with upi app select

--- App.tsx
+++ App.tsx
@@ -5,23 +5,106 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Linking,
+  StyleSheet,
+  Alert,
+} from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [toUpi, setToUpi] = useState('');
+  const [desc, setDesc] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const handlePayment = () => {
+    if (!toUpi || !amount) {
+      Alert.alert('Error', 'Please enter UPI ID and amount.');
+      return;
+    }
+
+    const uri = `upi://pay?pa=${toUpi}&pn=Recipient&tn=${desc}&am=${amount}&cu=INR`;
+
+    Linking.canOpenURL(uri)
+      .then(supported => {
+        if (!supported) {
+          Alert.alert('Error', 'No UPI apps found. Install one to proceed.');
+        } else {
+          return Linking.openURL(uri);
+        }
+      })
+      .catch(err => Alert.alert('Error', err.message));
+  };
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text style={styles.title}>UPI Payment</Text>
+      
+      <Text style={styles.label}>To UPI ID</Text>
+      <TextInput
+        placeholder="Enter UPI ID"
+        value={toUpi}
+        onChangeText={setToUpi}
+        style={styles.input}
+      />
+      
+      <Text style={styles.label}>Description</Text>
+      <TextInput
+        placeholder="Enter payment description"
+        value={desc}
+        onChangeText={setDesc}
+        style={styles.input}
+      />
+      
+      <Text style={styles.label}>Amount (INR)</Text>
+      <TextInput
+        placeholder="Enter amount"
+        value={amount}
+        onChangeText={setAmount}
+        keyboardType="numeric"
+        style={styles.input}
+      />
+      <Button title="Send" onPress={handlePayment} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    padding: 20,
+  },
+  input: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    color: '#333',
+    width: '80%',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 30,
+    textAlign: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  label: {
+    fontSize: 16,
+    color: '#ffffff',
+    marginBottom: 5,
+    width: '80%',
+    textAlign: 'left',
+    fontWeight: '500',
   },
 });
 
